01ba6fc0cee8a4829509661b04059d8a61df1b79
Jubayer Juhan <davidjuhan23@gmail.com>
2022-09-05 00:03:13 +0600

    Firebase error

--- src/components/Login/Login/Login.js
+++ src/components/Login/Login/Login.js
@@ -1,139 +1,161 @@
-import React, { useContext, useState } from 'react';
-import { Link, useHistory, useLocation } from 'react-router-dom';
-import logo from '../../../fakeData/images/Logo/projectlogohead.png';
-import './Login.css';
-import { Button, Form } from 'react-bootstrap';
-import {  initializeLoginFramework, signInWithEmailAndPassword } from '../firebase/loginManager';
-import { UserContext } from '../../../App';
+import React, { useContext, useState } from "react";
+import { Link, useHistory, useLocation } from "react-router-dom";
+import logo from "../../../fakeData/images/Logo/projectlogohead.png";
+import "./Login.css";
+import { Button, Form } from "react-bootstrap";
+import {
+  initializeLoginFramework,
+  signInWithEmailAndPassword,
+} from "../firebase/loginManager";
+import { UserContext } from "../../../App";
 
 const Login = () => {
-    // const [newUser, setNewUser] = useState(false);
-    const [user, setUser] = useState({
-        isSignedIn: false,
-        email: '',
-        password: '',
-        error: '',
-        success: false
-    });
-    const [msg, setMsg] = useState('');
-    const [btn, setBtn] = useState(true);
+  // const [newUser, setNewUser] = useState(false);
+  const [user, setUser] = useState({
+    isSignedIn: false,
+    email: "",
+    password: "",
+    error: "",
+    success: false,
+  });
+  const [msg, setMsg] = useState("");
+  const [btn, setBtn] = useState(true);
 
-    initializeLoginFramework();
-    const [, setLoggedInUser] = useContext(UserContext);
-    const history = useHistory();
-    const location = useLocation();
-    let { from } = location.state || { from: { pathname: "/" } };
+  initializeLoginFramework();
+  const [, setLoggedInUser] = useContext(UserContext);
+  const history = useHistory();
+  const location = useLocation();
+  let { from } = location.state || { from: { pathname: "/" } };
 
+  // const signOut = () => {
+  //     handleSignOut()
+  //         .then(res => {
+  //             handleResponse(res, false);
+  //         })
+  // }
 
-    // const signOut = () => {
-    //     handleSignOut()
-    //         .then(res => {
-    //             handleResponse(res, false);
-    //         })
-    // }
-
-    const blur = (e) => {
-        if (e.target.name === 'email') {
-            const emailValid = /\S+@\S+\.\S+/.test(e.target.value);
-            if (!emailValid) {
-                setMsg('Email is not valid');
-                setBtn(true);
-            }
-        }
-
-        if (e.target.name === 'password') {
-            const isPasswordValid = e.target.value.length > 6;
-            const passwordHasNumber = /\d{1}/.test(e.target.value);
-            if (!passwordHasNumber || !isPasswordValid) {
-                setMsg('Password is not valid');
-                setBtn(true);
-            }
-        }
+  const blur = (e) => {
+    if (e.target.name === "email") {
+      const emailValid = /\S+@\S+\.\S+/.test(e.target.value);
+      if (!emailValid) {
+        setMsg("Email is not valid");
+        setBtn(true);
+      }
     }
 
-    const handleChange = (e) => {
-        // console.log(e.target.name, e.target.value)
+    if (e.target.name === "password") {
+      const isPasswordValid = e.target.value.length > 6;
+      const passwordHasNumber = /\d{1}/.test(e.target.value);
+      if (!passwordHasNumber || !isPasswordValid) {
+        setMsg("Password is not valid");
+        setBtn(true);
+      }
+    }
+  };
 
-        let isFieldValid = true;
-        if (e.target.name === 'email') {
-            const emailValid = /\S+@\S+\.\S+/.test(e.target.value);
+  const handleChange = (e) => {
+    // console.log(e.target.name, e.target.value)
 
-            isFieldValid = emailValid;
-        }
+    let isFieldValid = true;
+    if (e.target.name === "email") {
+      const emailValid = /\S+@\S+\.\S+/.test(e.target.value);
 
-        if (e.target.name === 'password') {
-            const isPasswordValid = e.target.value.length > 6;
-            const passwordHasNumber = /\d{1}/.test(e.target.value);
-
-            isFieldValid = isPasswordValid && passwordHasNumber;
-        }
-        if (isFieldValid) {
-            const newUserInfo = { ...user };
-            newUserInfo[e.target.name] = e.target.value;
-            setUser(newUserInfo);
-            setBtn(false);
-            setMsg('');
-            // console.log(newUserInfo);
-        }
+      isFieldValid = emailValid;
     }
 
-    const handleSubmit = (e) => {
-        // console.log(user.email, user.password)
+    if (e.target.name === "password") {
+      const isPasswordValid = e.target.value.length > 6;
+      const passwordHasNumber = /\d{1}/.test(e.target.value);
 
-
-        if (user.email && user.password) {
-            signInWithEmailAndPassword(user.email, user.password)
-                .then(res => {
-                    if (res.success) {
-                        handleResponse(res, true);
-
-                    }
-                    setMsg(res.error)
-                    // console.log(res.success);
-                })
-        }
-        e.preventDefault();
+      isFieldValid = isPasswordValid && passwordHasNumber;
     }
-
-    const handleResponse = (res, redirect) => {
-        setUser(res);
-        setLoggedInUser(res);
-        if (redirect) {
-            history.replace(from);
-        }
+    if (isFieldValid) {
+      const newUserInfo = { ...user };
+      newUserInfo[e.target.name] = e.target.value;
+      setUser(newUserInfo);
+      setBtn(false);
+      setMsg("");
+      // console.log(newUserInfo);
     }
+  };
 
+  const handleSubmit = (e) => {
+    // console.log(user.email, user.password)
 
-    return (
-        <div className="login-container">
-            <div style={{ position: 'relative', top: '120px' }} className="text-center container">
-                <Link to='/home'>
-                    <img width='180px' src={logo} alt="" />
-                </Link>
-                <div className="mt-5  d-flex justify-content-center">
+    if (user.email && user.password) {
+      signInWithEmailAndPassword(user.email, user.password).then((res) => {
+        if (res.success) {
+          handleResponse(res, true);
+        }
+        setMsg(res.error);
+        // console.log(res.success);
+      });
+    }
+    e.preventDefault();
+  };
 
-                    <Form onSubmit={handleSubmit} style={{ width: '450px' }}>
-                        <p style={{ fontSize: '20px', color: 'blue' }} ><b>{msg}</b></p>
-                        <Form.Group className="mt-4" controlId="formBasicEmail">
-                            <Form.Control onBlur={blur} onChange={handleChange} style={{ height: "60px", background: '#F5F5F5' }} name="email" type="email" placeholder="Email" />
-                        </Form.Group>
+  const handleResponse = (res, redirect) => {
+    setUser(res);
+    setLoggedInUser(res);
+    if (redirect) {
+      history.replace(from);
+    }
+  };
 
-                        <Form.Group className="mt-4" controlId="formBasicPassword">
-                            <Form.Control onBlur={blur} onChange={handleChange} style={{ height: "60px", background: '#F5F5F5' }} name="password" type="password" placeholder="Password" />
-                        </Form.Group>
+  return (
+    <div className="login-container">
+      <div
+        style={{ position: "relative", top: "120px" }}
+        className="text-center container"
+      >
+        <Link to="/home">
+          <img width="180px" src={logo} alt="" />
+        </Link>
+        <div className="mt-5  d-flex justify-content-center">
+          <Form onSubmit={handleSubmit} style={{ width: "450px" }}>
+            <p style={{ fontSize: "20px", color: "blue" }}>
+              <b>{msg}</b>
+            </p>
+            <Form.Group className="mt-4" controlId="formBasicEmail">
+              <Form.Control
+                onBlur={blur}
+                onChange={handleChange}
+                style={{ height: "60px", background: "#F5F5F5" }}
+                name="email"
+                type="email"
+                placeholder="Email"
+              />
+            </Form.Group>
 
+            <Form.Group className="mt-4" controlId="formBasicPassword">
+              <Form.Control
+                onBlur={blur}
+                onChange={handleChange}
+                style={{ height: "60px", background: "#F5F5F5" }}
+                name="password"
+                type="password"
+                placeholder="Password"
+              />
+            </Form.Group>
 
-                        <Button disabled={btn} style={{ padding: '10px 195px' }} variant="primary" type="submit">
-                            Sign in</Button>
-                        <br />
-                        <br />
-                        <Link to="/signup" style={{ color: 'blue' }}>Create an Account</Link>
-                    </Form>
-
-                </div>
-            </div>
+            <Button
+              disabled={btn}
+              style={{ padding: "10px 195px" }}
+              variant="primary"
+              type="submit"
+            >
+              Sign in
+            </Button>
+            <br />
+            <br />
+            <Link to="/signup" style={{ color: "blue" }}>
+              Create an Account
+            </Link>
+          </Form>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Login;
